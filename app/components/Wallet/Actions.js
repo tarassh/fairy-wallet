1afd32e7038127400414df07cb4d891261df496d
Taras Shchybovyk <tshchybo@gmail.com>
2018-07-12 13:52:05 +0300

    Remove receive tab

--- app/components/Wallet/Actions.js
+++ app/components/Wallet/Actions.js
@@ -1,26 +1,23 @@
 // @flow
 import React, { Component } from 'react';
-import { render } from 'react-dom';
-import { Tab, Segment } from 'semantic-ui-react';
+import { Tab } from 'semantic-ui-react';
 import ActionsHistory from '../../containers/Wallet/Actions/History';
 import ActionsSend from '../../containers/Wallet/Actions/Send';
-import ActionsReceive from './Actions/Receive';
 
 type Props = {
 };
 
 export default class Actions extends Component<Props> {
-    props: Props;
-    
-    render() {
-        const panes = [
-            { menuItem: 'History', render: () => <ActionsHistory /> },
-            { menuItem: 'Send', render: () => <ActionsSend /> },
-            { menuItem: 'Receive', render: () => <ActionsReceive /> },
-        ]
+  props: Props;
 
-        return (
-            <Tab menu={{ secondary: true, pointing: true }} panes={panes} />
-        );
-    }
+  render() {
+    const panes = [
+      { menuItem: 'History', render: () => <ActionsHistory /> },
+      { menuItem: 'Send', render: () => <ActionsSend /> }
+    ]
+
+    return (
+      <Tab menu={{ secondary: true, pointing: true }} panes={panes} />
+    );
+  }
 }
