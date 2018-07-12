8c19267c8e371f0a4fff9ad81c31fc6037ac73f8
Taras Shchybovyk <tshchybo@gmail.com>
2018-07-12 12:09:28 +0300

    Remove redundant code in order to remove crashes.

--- app/components/Wallet/Actions/History.js
+++ app/components/Wallet/Actions/History.js
@@ -1,66 +1,59 @@
 // @flow
 import React, { Component } from 'react';
-import { render } from 'react-dom';
 import { Tab, Segment, Table } from 'semantic-ui-react';
 import _ from 'lodash'
 
 type Props = {
-    history: {},
-    accounts: {}
+  accounts: {}
 };
 
 export default class History extends Component<Props> {
-    props: Props;
-    
-    render() {
-        const {
-            history,
-            accounts,
-            actions
-        } = this.props;
+  props: Props;
 
-        const tableData = [
-            { currency: 'EOS', total: 15000 },
-            { currency: 'Amber', total: 40000  },
-            { currency: 'ShittyToken', total: 2500  },
-            { currency: 'EosDolliar', total: 700000 },
-        ]
-    
-        const content = 
-            <Segment className='no-border'>
-                <Table celled basic='very' compact='very' unstackable>
-                    <Table.Header>
-                      <Table.Row>
-                        <Table.HeaderCell>
-                          Account
-                        </Table.HeaderCell>
-                        <Table.HeaderCell>
-                          Name
-                        </Table.HeaderCell>
-                        <Table.HeaderCell>
-                          Time
-                        </Table.HeaderCell>
-                        <Table.HeaderCell>
-                          Name
-                        </Table.HeaderCell>
-                        <Table.HeaderCell>
-                          Name
-                        </Table.HeaderCell>
-                      </Table.Row>
-                    </Table.Header>
-                    <Table.Body>
-                      {_.map(tableData, ({currency, total}) => (
-                        <Table.Row key={currency}>
-                          <Table.Cell collapsing>{currency}</Table.Cell>
-                          <Table.Cell collapsing>{total}</Table.Cell>
-                        </Table.Row>
-                      ))}
-                    </Table.Body>
-                </Table>
-            </Segment>;
-        
-        return (
-            <Tab.Pane content={content} className='history' />
-        );
-    }
+  render() {
+
+    const tableData = [
+      { currency: 'EOS', total: 15000 },
+      { currency: 'Amber', total: 40000 },
+      { currency: 'ShittyToken', total: 2500 },
+      { currency: 'EosDolliar', total: 700000 },
+    ]
+
+    const content =
+      (<Segment className='no-border'>
+        <Table celled basic='very' compact='very' unstackable>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>
+                Account
+              </Table.HeaderCell>
+              <Table.HeaderCell>
+                Name
+              </Table.HeaderCell>
+              <Table.HeaderCell>
+                Time
+              </Table.HeaderCell>
+              <Table.HeaderCell>
+                Name
+              </Table.HeaderCell>
+              <Table.HeaderCell>
+                Name
+              </Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {_.map(tableData, ({ currency, total }) => (
+              <Table.Row key={currency}>
+                <Table.Cell collapsing>{currency}</Table.Cell>
+                <Table.Cell collapsing>{total}</Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table>
+       </Segment>);
+
+    return (
+      <Tab.Pane content={content} className='history' />
+    );
+  }
 }
--- app/containers/Wallet/Actions/History.js
+++ app/containers/Wallet/Actions/History.js
@@ -1,54 +1,48 @@
 // @flow
 import React, { Component } from 'react';
-import { render } from 'react-dom';
-import { getActions } from '../../../actions/accounts';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import History from '../../../components/Wallet/Actions/History';
+import { getActions } from '../../../actions/accounts';
 
 type Props = {
-    history: {},
-    accounts: {},
-    actions: {}
+  accounts: {},
+  actions: {}
 };
 
 class HistoryContainer extends Component<Props> {
-    props: Props;
-    
-    componentDidMount(){
-        const {
-            actions,
-            accounts
-        } = this.props;
+  props: Props;
 
-        const name = accounts.account.account_name;
-        actions.getActions(name); 
-    }
-    
-    render() {
-        const {
-            history
-        } = this.props;
-        
-        return (
-            <History />
-        );
-    }
-}
-            
-function mapStateToProps(state){
-      return {
-          history: state.actions,
-          accounts: state.accounts
-      }      
+  componentDidMount() {
+    const {
+      actions,
+      accounts
+    } = this.props;
+
+    const name = accounts.account.account_name;
+    actions.getActions(name);
+  }
+
+  render() {
+    const { accounts } = this.props;
+    return (
+      <History accounts={accounts} />
+    );
+  }
 }
 
-function mapDispatchToProps(dispatch){
-    return { 
-        actions: bindActionCreators({
-            getActions: getActions
-        }, dispatch)       
-    };  
+function mapStateToProps(state) {
+  return {
+    accounts: state.accounts
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators({
+      getActions
+    }, dispatch)
+  };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(HistoryContainer);
