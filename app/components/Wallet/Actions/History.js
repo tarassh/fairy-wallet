bf2d1bbdfdf36e34191e805aff33214b8106cd17
Taras Shchybovyk <tshchybo@gmail.com>
2018-07-21 17:07:11 +0300

    History placeholder.

--- app/components/Wallet/Actions/History.js
+++ app/components/Wallet/Actions/History.js
@@ -1,7 +1,6 @@
 // @flow
 import React, { Component } from 'react';
-import { Tab, Segment, Table } from 'semantic-ui-react';
-import _ from 'lodash'
+import { Tab, Segment, List } from 'semantic-ui-react';
 
 type Props = {};
 
@@ -9,50 +8,293 @@ export default class History extends Component<Props> {
   props: Props;
 
   render() {
-
-    const tableData = [
-      { currency: 'EOS', total: 15000 },
-      { currency: 'Amber', total: 40000 },
-      { currency: 'ShittyToken', total: 2500 },
-      { currency: 'EosDolliar', total: 700000 },
-    ]
+    const options = {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric'
+    };
+    const options2 = { hour: '2-digit', minute: '2-digit' };
+    const today = new Date();
 
     const content = (
-      <Segment className='no-border'>
-        <Table celled basic='very' compact='very' unstackable>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell>
-                Account
-              </Table.HeaderCell>
-              <Table.HeaderCell>
-                Name
-              </Table.HeaderCell>
-              <Table.HeaderCell>
-                Time
-              </Table.HeaderCell>
-              <Table.HeaderCell>
-                Name
-              </Table.HeaderCell>
-              <Table.HeaderCell>
-                Name
-              </Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            {_.map(tableData, ({ currency, total }) => (
-              <Table.Row key={currency}>
-                <Table.Cell collapsing>{currency}</Table.Cell>
-                <Table.Cell collapsing>{total}</Table.Cell>
-              </Table.Row>
-            ))}
-          </Table.Body>
-        </Table>
+      <Segment className="no-border no-padding">
+        <List>
+          <List.Item>
+            <p>{today.toLocaleDateString('en-US', options)}</p>
+            <List.Content>
+              <List selection relaxed divided>
+                <List.Item>
+                  <List.Content>
+                    {`${today.toLocaleTimeString(
+                      'en-US',
+                      options2
+                    )} Transaction id , name, paramenters`}
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Content>
+                    {`${today.toLocaleTimeString(
+                      'en-US',
+                      options2
+                    )} Transaction id , name, paramenters`}
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Content>
+                    {`${today.toLocaleTimeString(
+                      'en-US',
+                      options2
+                    )} Transaction id , name, paramenters`}
+                  </List.Content>
+                </List.Item>
+              </List>
+            </List.Content>
+          </List.Item>
+
+          <List.Item>
+            <p>{today.toLocaleDateString('en-US', options)}</p>
+            <List.Content>
+              <List selection relaxed divided>
+                <List.Item>
+                  <List.Content>
+                    {`${today.toLocaleTimeString(
+                      'en-US',
+                      options2
+                    )} Transaction id , name, paramenters`}
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Content>
+                    {`${today.toLocaleTimeString(
+                      'en-US',
+                      options2
+                    )} Transaction id , name, paramenters`}
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Content>
+                    {`${today.toLocaleTimeString(
+                      'en-US',
+                      options2
+                    )} Transaction id , name, paramenters`}
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Content>
+                    {`${today.toLocaleTimeString(
+                      'en-US',
+                      options2
+                    )} Transaction id , name, paramenters`}
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Content>
+                    {`${today.toLocaleTimeString(
+                      'en-US',
+                      options2
+                    )} Transaction id , name, paramenters`}
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Content>
+                    {`${today.toLocaleTimeString(
+                      'en-US',
+                      options2
+                    )} Transaction id , name, paramenters`}
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Content>
+                    {`${today.toLocaleTimeString(
+                      'en-US',
+                      options2
+                    )} Transaction id , name, paramenters`}
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Content>
+                    {`${today.toLocaleTimeString(
+                      'en-US',
+                      options2
+                    )} Transaction id , name, paramenters`}
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Content>
+                    {`${today.toLocaleTimeString(
+                      'en-US',
+                      options2
+                    )} Transaction id , name, paramenters`}
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Content>
+                    {`${today.toLocaleTimeString(
+                      'en-US',
+                      options2
+                    )} Transaction id , name, paramenters`}
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Content>
+                    {`${today.toLocaleTimeString(
+                      'en-US',
+                      options2
+                    )} Transaction id , name, paramenters`}
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Content>
+                    {`${today.toLocaleTimeString(
+                      'en-US',
+                      options2
+                    )} Transaction id , name, paramenters`}
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Content>
+                    {`${today.toLocaleTimeString(
+                      'en-US',
+                      options2
+                    )} Transaction id , name, paramenters`}
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Content>
+                    {`${today.toLocaleTimeString(
+                      'en-US',
+                      options2
+                    )} Transaction id , name, paramenters`}
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Content>
+                    {`${today.toLocaleTimeString(
+                      'en-US',
+                      options2
+                    )} Transaction id , name, paramenters`}
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Content>
+                    {`${today.toLocaleTimeString(
+                      'en-US',
+                      options2
+                    )} Transaction id , name, paramenters`}
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Content>
+                    {`${today.toLocaleTimeString(
+                      'en-US',
+                      options2
+                    )} Transaction id , name, paramenters`}
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Content>
+                    {`${today.toLocaleTimeString(
+                      'en-US',
+                      options2
+                    )} Transaction id , name, paramenters`}
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Content>
+                    {`${today.toLocaleTimeString(
+                      'en-US',
+                      options2
+                    )} Transaction id , name, paramenters`}
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Content>
+                    {`${today.toLocaleTimeString(
+                      'en-US',
+                      options2
+                    )} Transaction id , name, paramenters`}
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Content>
+                    {`${today.toLocaleTimeString(
+                      'en-US',
+                      options2
+                    )} Transaction id , name, paramenters`}
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Content>
+                    {`${today.toLocaleTimeString(
+                      'en-US',
+                      options2
+                    )} Transaction id , name, paramenters`}
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Content>
+                    {`${today.toLocaleTimeString(
+                      'en-US',
+                      options2
+                    )} Transaction id , name, paramenters`}
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Content>
+                    {`${today.toLocaleTimeString(
+                      'en-US',
+                      options2
+                    )} Transaction id , name, paramenters`}
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Content>
+                    {`${today.toLocaleTimeString(
+                      'en-US',
+                      options2
+                    )} Transaction id , name, paramenters`}
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Content>
+                    {`${today.toLocaleTimeString(
+                      'en-US',
+                      options2
+                    )} Transaction id , name, paramenters`}
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Content>
+                    {`${today.toLocaleTimeString(
+                      'en-US',
+                      options2
+                    )} Transaction id , name, paramenters`}
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Content>
+                    {`${today.toLocaleTimeString(
+                      'en-US',
+                      options2
+                    )} Transaction id , name, paramenters`}
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Content>
+                    {`${today.toLocaleTimeString(
+                      'en-US',
+                      options2
+                    )} Transaction id , name, paramenters`}
+                  </List.Content>
+                </List.Item>
+              </List>
+            </List.Content>
+          </List.Item>
+        </List>
       </Segment>
     );
 
-    return (
-      <Tab.Pane content={content} className='history' />
-    );
+    return <Tab.Pane content={content} className="history" />;
   }
 }
