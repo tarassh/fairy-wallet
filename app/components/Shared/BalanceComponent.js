338b43388ab54f22104661ed01dcf072e0a13093
Taras Shchybovyk <tshchybo@gmail.com>
2018-07-21 21:59:58 +0300

    Add unstaking components.

--- app/components/Shared/BalanceComponent.js
+++ app/components/Shared/BalanceComponent.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Button, Label, Icon } from 'semantic-ui-react';
 
+const pretty = require('pretty-time');
+
 type Props = {
   account: {}
 };
@@ -11,6 +13,14 @@ class BalanceComponent extends Component<Props> {
     const staked = `${parseFloat(account.voter_info.staked / 10000).toFixed(
       4
     )} EOS`;
+    const { refund_request } = account; // eslint-disable-line camelcase
+
+    const refundTotal = totalRefund(refund_request);
+    let timeLeft = 0;
+    if (refundTotal > 0) {
+      timeLeft = new Date(refund_request.request_time);
+      timeLeft.setDate(timeLeft.getDate() + 3);
+    }
 
     return (
       <Button as="div" labelPosition="right">
@@ -19,13 +29,37 @@ class BalanceComponent extends Component<Props> {
           <Icon name="lock open" color="grey" />
           {account.core_liquid_balance}
         </Label>
-        <Label as="div" basic>
+        <Label
+          as="div"
+          basic
+          style={refundTotal > 0 ? { borderRadius: 'unset' } : {}}
+        >
           <Icon name="lock" color="grey" />
           {staked}
         </Label>
+        {refundTotal > 0 && (
+          <Label as="div" basic>
+            <Icon name="clock" color="grey" />
+            {`${refundTotal.toFixed(4)} EOS [${pretty(
+              timeLeft.getTime() * 100
+            )}]`}
+          </Label>
+        )}
       </Button>
     );
   }
+}
+
+function totalRefund(request) {
+  if (request && request !== null) {
+    return assetToFloat(request.cpu_amount) + assetToFloat(request.net_amount);
+  }
+  return 0;
+}
+
+function assetToFloat(asset) {
+  const [amount] = asset.split(' ');
+  return parseFloat(amount);
 }
 
 export default BalanceComponent;
