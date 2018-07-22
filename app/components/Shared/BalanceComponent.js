32dd2552e85ddab437a8faa6890736bab4b687db
Taras Shchybovyk <tshchybo@gmail.com>
2018-07-22 13:13:28 +0300

    Add Popup.

--- app/components/Shared/BalanceComponent.js
+++ app/components/Shared/BalanceComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Label, Icon } from 'semantic-ui-react';
+import { Button, Label, Icon, Popup, Header, Table } from 'semantic-ui-react';
 
 const pretty = require('pretty-time');
 
@@ -10,44 +10,97 @@ type Props = {
 class BalanceComponent extends Component<Props> {
   render() {
     const { account } = this.props;
-    const staked = `${parseFloat(account.voter_info.staked / 10000).toFixed(
-      4
-    )} EOS`;
-    const { refund_request } = account; // eslint-disable-line camelcase
 
-    const refundTotal = totalRefund(refund_request);
-    let timeLeft = 0;
-    if (refundTotal > 0) {
+    let timeLeft = '';
+
+    const { total, liquid, staked, unstaking } = balanceStats(account);
+    if (unstaking) {
+      const { refund_request } = account; // eslint-disable-line camelcase
       timeLeft = new Date(refund_request.request_time);
       timeLeft.setDate(timeLeft.getDate() + 3);
+      timeLeft = pretty(timeLeft.getTime() * 100);
     }
 
+    const content = (
+      <div>
+        <Header as="h5" content="EOS Balance" />
+        <Table basic="very">
+          <Table.Body>
+            <Table.Row>
+              <Table.Cell textAlign="left">Total</Table.Cell>
+              <Table.Cell textAlign="right">{total}</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell textAlign="left">Liquid</Table.Cell>
+              <Table.Cell textAlign="right">{liquid}</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell textAlign="left">Staked</Table.Cell>
+              <Table.Cell textAlign="right">{staked}</Table.Cell>
+            </Table.Row>
+            {unstaking && (
+              <Table.Row>
+                <Table.Cell>Unstaking</Table.Cell>
+                <Table.Cell>{`${timeLeft} ${unstaking}`}</Table.Cell>
+              </Table.Row>
+            )}
+          </Table.Body>
+        </Table>
+      </div>
+    );
+
     return (
-      <Button as="div" labelPosition="right">
-        <Button icon="dollar" basic />
-        <Label as="div" basic style={{ borderRadius: 'unset' }}>
-          <Icon name="lock open" color="grey" />
-          {account.core_liquid_balance}
-        </Label>
-        <Label
-          as="div"
-          basic
-          style={refundTotal > 0 ? { borderRadius: 'unset' } : {}}
-        >
-          <Icon name="lock" color="grey" />
-          {staked}
-        </Label>
-        {refundTotal > 0 && (
-          <Label as="div" basic>
-            <Icon name="clock" color="grey" />
-            {`${refundTotal.toFixed(4)} EOS [${pretty(
-              timeLeft.getTime() * 100
-            )}]`}
-          </Label>
-        )}
-      </Button>
+      <div>
+        <Popup
+          content={content}
+          on="click"
+          trigger={
+            <Button as="div" labelPosition="right" onClick={this.toggle}>
+              <Button icon="dollar" basic />
+              <Label as="div" basic style={{ borderRadius: 'unset' }}>
+                <Icon name="lock open" color="grey" />
+                {liquid}
+              </Label>
+              <Label
+                as="div"
+                basic
+                style={unstaking ? { borderRadius: 'unset' } : {}}
+              >
+                <Icon name="lock" color="grey" />
+                {staked}
+              </Label>
+              {!!unstaking && (
+                <Label as="div" basic>
+                  <Icon name="clock" color="grey" />
+                  {`${unstaking} [${timeLeft}]`}
+                </Label>
+              )}
+            </Button>
+          }
+        />
+      </div>
     );
   }
+}
+
+function balanceStats(account) {
+  const staked = account.voter_info.staked / 10000;
+  const unstaking = totalRefund(account.refund_request);
+  const liquid = assetToFloat(account.core_liquid_balance);
+  const total = staked + unstaking + liquid;
+
+  const stats = {
+    total: `${parseFloat(total).toFixed(4)} EOS`,
+    liquid: `${parseFloat(liquid).toFixed(4)} EOS`,
+    staked: `${parseFloat(staked).toFixed(4)} EOS`
+  };
+
+  if (unstaking > 0) {
+    Object.assign(stats, {
+      unstaking: `${parseFloat(unstaking).toFixed(4)} EOS`
+    });
+  }
+  return stats;
 }
 
 function totalRefund(request) {
--- app/components/Wallet/StakedStats.js
+++ app/components/Wallet/StakedStats.js
@@ -11,35 +11,66 @@ class StakedStats extends Component<Props> {
   render() {
     const { account } = this.props;
     const {
-      cpu_limit,                    // eslint-disable-line camelcase
-      net_limit,                    // eslint-disable-line camelcase
-      self_delegated_bandwidth,     // eslint-disable-line camelcase
-      total_resources               // eslint-disable-line camelcase
+      cpu_limit, // eslint-disable-line camelcase
+      net_limit, // eslint-disable-line camelcase
+      self_delegated_bandwidth, // eslint-disable-line camelcase
+      total_resources // eslint-disable-line camelcase
     } = account;
 
     return (
       <div>
-        <Header size='tiny' textAlign='center' dividing style={{ marginBottom: '0em', marginTop: '1em'}}>
+        <Header
+          size="tiny"
+          textAlign="center"
+          dividing
+          style={{ marginBottom: '0em', marginTop: '1em' }}
+        >
           CPU
         </Header>
-        <Table basic='very' style={{ marginTop: '0em'}}>
+        <Table basic="very" style={{ marginTop: '0em' }}>
           <TableBody>
-            <Table.Row textAlign='center'>
-              <Table.Cell width={4}>total<p>{total_resources.cpu_weight}</p></Table.Cell>
-              <Table.Cell width={4}>staked<p>{self_delegated_bandwidth.cpu_weight}</p></Table.Cell>
-              <Table.Cell width={4}>usage<p>{(cpu_limit.used / 1000000).toFixed(4)} sec / {(cpu_limit.max / 1000000).toFixed(4)} sec</p></Table.Cell>
+            <Table.Row textAlign="center">
+              <Table.Cell width={4}>
+                total
+                <p>{total_resources.cpu_weight}</p>
+              </Table.Cell>
+              <Table.Cell width={4}>
+                staked
+                <p>{self_delegated_bandwidth.cpu_weight}</p>
+              </Table.Cell>
+              <Table.Cell width={4}>
+                usage
+                <p>
+                  {(cpu_limit.used / 1000000).toFixed(4)} sec /{' '}
+                  {(cpu_limit.max / 1000000).toFixed(4)} sec
+                </p>
+              </Table.Cell>
             </Table.Row>
           </TableBody>
         </Table>
-        <Header size='tiny' textAlign='center' dividing style={{ marginBottom: '0em', marginTop: '3em'}}>
+        <Header
+          size="tiny"
+          textAlign="center"
+          dividing
+          style={{ marginBottom: '0em', marginTop: '3em' }}
+        >
           Network
         </Header>
-        <Table basic='very' style={{ marginTop: '0em'}}>
+        <Table basic="very" style={{ marginTop: '0em' }}>
           <TableBody>
-            <Table.Row textAlign='center'>
-              <Table.Cell width={4}>total<p>{total_resources.net_weight}</p></Table.Cell>
-              <Table.Cell width={4}>staked<p>{self_delegated_bandwidth.net_weight}</p></Table.Cell>
-              <Table.Cell width={4}>usage<p>{pretty(parseInt(net_limit.used, 10))} / {pretty(parseInt(net_limit.max, 10))}</p></Table.Cell>
+            <Table.Row textAlign="center">
+              <Table.Cell width={4}>
+                total<p>{total_resources.net_weight}</p>
+              </Table.Cell>
+              <Table.Cell width={4}>
+                staked<p>{self_delegated_bandwidth.net_weight}</p>
+              </Table.Cell>
+              <Table.Cell width={4}>
+                usage<p>
+                  {pretty(parseInt(net_limit.used, 10))} /{' '}
+                  {pretty(parseInt(net_limit.max, 10))}
+                </p>
+              </Table.Cell>
             </Table.Row>
           </TableBody>
         </Table>
