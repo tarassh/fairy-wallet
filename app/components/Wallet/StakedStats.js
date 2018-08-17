0daa1f183975371d5c3f5a94a6aaedd7e5b6f563
Taras Shchybovyk <tshchybo@gmail.com>
2018-08-17 15:49:51 +0300

    Updated Stated

--- app/components/Wallet/StakedStats.js
+++ app/components/Wallet/StakedStats.js
@@ -1,13 +1,8 @@
 // @flow
 import React, { Component } from 'react';
 import _ from 'lodash';
-import { Table, TableBody, Header, Segment } from 'semantic-ui-react';
-import { numberToPrettyAsset, assetToNumber } from '../../utils/asset';
-import UtilityChart from '../Shared/UtilityChart';
+import { assetToNumber } from '../../utils/asset';
 import StakeChart from '../Shared/StakeChart';
-
-const pretty = require('prettysize');
-const numeral = require('numeral');
 
 type Props = {
   account: {},
@@ -16,7 +11,7 @@ type Props = {
 };
 
 class StakedStats extends Component<Props> {
-  renderDelegate() {
+  render() {
     const { delegates, account, delegatee } = this.props;
     const refunds = unstakingBalance(account);
 
@@ -33,6 +28,10 @@ class StakedStats extends Component<Props> {
 
     return (
       <div>
+        <p className="title">Staked Information</p>
+        <p className="subtitle">CPU and Netowrk bandwidth delegation</p>
+        <br />
+
         <StakeChart stakes={cpuStakes} max={cpuStake + cpuRefund} active={active} />
         <p>Staked CPU {parseFloat(cpuStake).toFixed(4)}</p>
         
@@ -47,156 +46,6 @@ class StakedStats extends Component<Props> {
       </div>
     )
   }
-
-  render() {
-    const { account } = this.props;
-    const { totalResources, selfDelegated, usage, ram } = formatStats(account);
-
-    return (
-      <Segment>
-        {this.renderDelegate()}
-        <Header
-          size="tiny"
-          textAlign="center"
-          style={{ marginBottom: '0em', marginTop: '0em' }}
-        >
-          <h5>CPU</h5>
-          <UtilityChart stats={{used: account.cpu_limit.used, max: account.cpu_limit.max }} />
-        </Header>
-        <Table basic="very" style={{ marginTop: '0em' }}>
-          <TableBody>
-            <Table.Row textAlign="center">
-              {totalResources && (
-                <Table.Cell width={4}>
-                  <h5>Total</h5>
-                  <h5>{totalResources.cpu}</h5>
-                </Table.Cell>
-              )}
-              {selfDelegated && (
-                <Table.Cell width={4}>
-                  <h5>Staked</h5>
-                  <h5>{selfDelegated.cpu} </h5>
-                </Table.Cell>
-              )}
-              {usage && (
-                <Table.Cell width={4}>
-                  <h5>Used</h5>
-                  <h5>{usage.cpu}</h5>
-                </Table.Cell>
-              )}
-            </Table.Row>
-          </TableBody>
-        </Table>
-        <Header
-          size="tiny"
-          textAlign="center"
-          style={{ marginBottom: '0em', marginTop: '0.5em' }}
-        >
-          <h5>Network</h5>
-          <UtilityChart stats={{used: account.net_limit.used, max: account.net_limit.max }} />
-        </Header>
-        <Table basic="very" style={{ marginTop: '0em' }}>
-          <TableBody>
-            <Table.Row textAlign="center">
-              {totalResources && (
-                <Table.Cell width={4}>
-                  <h5>Total</h5>
-                  <h5>{totalResources.net}</h5>
-                </Table.Cell>
-              )}
-              {selfDelegated && (
-                <Table.Cell width={4}>
-                  <h5>Staked</h5>
-                  <h5>{selfDelegated.net}</h5>
-                </Table.Cell>
-              )}
-              {usage && (
-                <Table.Cell width={4}>
-                  <h5>Used</h5>
-                  <h5>{usage.net}</h5>
-                </Table.Cell>
-              )}
-            </Table.Row>
-          </TableBody>
-        </Table>
-        <Header
-          size="tiny"
-          textAlign="center"
-          style={{ marginBottom: '0em', marginTop: '0.5em' }}
-        >
-          <h5>RAM</h5>
-          <UtilityChart stats={{used: account.ram_usage, max: account.ram_quota }} />
-        </Header>
-        <Table basic="very" style={{ marginTop: '0em', marginBottom: '0em' }}>
-          <TableBody>
-            <Table.Row textAlign="center">
-              {ram && (
-                <Table.Cell width={16}>
-                  <h5>Used</h5>
-                  <h5>{ram}</h5>
-                </Table.Cell>
-              )}
-            </Table.Row>
-          </TableBody>
-        </Table>
-      </Segment>
-    );
-  }
-}
-
-function formatStats(account) {
-  const {
-    cpu_limit, // eslint-disable-line camelcase
-    net_limit, // eslint-disable-line camelcase
-    self_delegated_bandwidth, // eslint-disable-line camelcase
-    total_resources, // eslint-disable-line camelcase
-    ram_quota, // eslint-disable-line camelcase
-    ram_usage // eslint-disable-line camelcase
-  } = account;
-
-  const result = {};
-
-  if (total_resources && total_resources !== null) {
-    // eslint-disable-line camelcase
-    const totalResources = {};
-    const cpu = assetToNumber(total_resources.cpu_weight);
-    const net = assetToNumber(total_resources.net_weight);
-    totalResources.cpu = numberToPrettyAsset(cpu);
-    totalResources.net = numberToPrettyAsset(net);
-    Object.assign(result, { totalResources });
-  }
-
-  if (self_delegated_bandwidth && self_delegated_bandwidth !== null) {
-    // eslint-disable-line camelcase
-    const selfDelegated = {};
-    const cpu = assetToNumber(self_delegated_bandwidth.cpu_weight);
-    const net = assetToNumber(self_delegated_bandwidth.net_weight);
-    selfDelegated.cpu = numberToPrettyAsset(cpu);
-    selfDelegated.net = numberToPrettyAsset(net);
-    Object.assign(result, { selfDelegated });
-  }
-
-  if (cpu_limit && cpu_limit !== null && net_limit && net_limit !== null) {
-    // eslint-disable-line camelcase
-    const usage = {};
-
-    const cpu = numeral(cpu_limit.used/cpu_limit.max).format('0.00%');
-
-    const net = `${pretty(parseInt(net_limit.used, 10))} / ${pretty(
-      parseInt(net_limit.max, 10)
-    )}`;
-    usage.cpu = cpu;
-    usage.net = net;
-    Object.assign(result, { usage });
-  }
-
-  if (ram_quota && ram_quota !== null && ram_usage && ram_usage !== null) {
-    // eslint-disable-line camelcase
-    const ram = `${pretty(ram_usage)} / ${pretty(ram_quota)}`;
-    Object.assign(result, { ram });
-  }
-
-  return result;
 }
 
 function unstakingBalance(account) {
