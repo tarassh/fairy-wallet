a8f1b9d095660f458becc643c02254cc679c2ba0
Taras Shchybovyk <tshchybo@gmail.com>
2018-08-19 13:46:20 +0300

    Redesign public key.

--- app/components/Shared/PublicKeyIcon.js
+++ app/components/Shared/PublicKeyIcon.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { Modal, Transition, Button, Image, Icon } from 'semantic-ui-react';
 import publicKeySvg from '../../../resources/images/verify-public-key.svg';
+import publicKeyErrorSvg from '../../../resources/images/verify-public-key-error.svg';
 
 import { getPublicKey } from '../../actions/ledger';
 
@@ -15,48 +16,127 @@ type Props = {
 };
 
 class PublicKeyComponent extends Component<Props> {
-  state = { opened: false };
+  state = {
+    opened: false,
+    step: 'open'
+  };
 
-  verifyPublicKey = () => {
-    this.props.getPublicKey(true);
+  openModality = () => {
     this.setState({ opened: true });
   };
 
-  handleClose = () => this.setState({ opened: false });
+  verifyPublicKey = () => {
+    this.setState({ step: 'verify' });
+    this.props.getPublicKey(true);
+  };
 
-  render() {
-    const { publicKey, loading, states } = this.props;
-    const { opened } = this.state;
+  handleClose = () => this.setState({ opened: false, step: 'open' });
 
-    let header = 'Copy public key';
-    let action;
-    let tryAgainAction;
-    let helperContent = (
+  renderStep0 = () => {
+    const header = <p className="title">Get Public Key</p>;
+    const content = (
       <div>
-        <Image
-          src={publicKeySvg}
-          centered
-          style={{ marginTop: '1em', marginBottom: '1em' }}
-        />
-        <p>Confirm that the public key on your device matches.</p>
-        <p style={{ userSelect: 'none' }}>{publicKey.wif}</p>
+        <p className="subtitle no-top-bottom-margin">
+          To get public key, you have to verify it on the device.
+        </p>
+        <p className="subtitle no-top-bottom-margin">
+          Carefully verify that it matches the public key on your computer.
+        </p>
       </div>
     );
-    if (opened && loading.PUBLIC_KEY_DISPLAY === false) {
+    const action = (
+      <div>
+        <Button content="Close" onClick={this.handleClose} />
+        <Button content="verify" onClick={this.verifyPublicKey} />
+      </div>
+    );
+
+    return (
+      <div>
+        {header}
+        <br />
+        {content}
+        <br />
+        <br />
+        {action}
+      </div>
+    );
+  };
+
+  renderStep1 = () => {
+    const { publicKey, loading, states } = this.props;
+    let header = <p className="title">Get Public Key</p>;
+    let image = publicKeySvg;
+    let desc = (
+      <div className="verify-content">
+        <p className="subtitle">EOS public key</p>
+        <p className="public-key">{publicKey.wif}</p>
+        <p className="device-hint">
+          Verify public key on your device. Press the right button to confirm.
+        </p>
+      </div>
+    );
+
+    let action;
+    if (loading.PUBLIC_KEY_DISPLAY === false) {
       if (states.displayPublicKey) {
+        image = publicKeySvg;
+
         action = (
           <CopyToClipboard text={publicKey.wif}>
             <Button content="Copy to clipborad" onClick={this.handleClose} />
           </CopyToClipboard>
         );
       } else {
-        action = <Button content="Close" onClick={this.handleClose} />;
-        tryAgainAction = (
-          <Button content="Try again" onClick={this.verifyPublicKey} />
+        header = <p className="title">Receive Public Key Rejected</p>;
+        image = publicKeyErrorSvg;
+
+        desc = (
+          <div>
+            <p className="subtitle no-top-bottom-margin">
+              Please try again or contact Wallet Support.
+            </p>
+          </div>
         );
-        helperContent = 'Please try again or contact wallet support';
-        header = 'Receive public key rejected';
+
+        action = (
+          <div>
+            <Button content="Close" onClick={this.handleClose} />
+            <Button content="Try again" onClick={this.verifyPublicKey} />
+          </div>
+        );
       }
+    }
+
+    const content = (
+      <div>
+        <Image
+          src={image}
+          centered
+          style={{ marginTop: '1em', marginBottom: '1em' }}
+        />
+        {desc}
+      </div>
+    );
+
+    return (
+      <div>
+        {header}
+        <br />
+        {content}
+        <br />
+        <br />
+        {action}
+      </div>
+    );
+  };
+
+  render() {
+    const { opened, step } = this.state;
+
+    let content = this.renderStep0();
+    if (step === 'verify') {
+      content = this.renderStep1();
     }
 
     return (
@@ -64,20 +144,13 @@ class PublicKeyComponent extends Component<Props> {
         icon
         basic
         circular
-        onClick={this.verifyPublicKey}
+        onClick={this.openModality}
         className="publickeycontainer"
       >
         <Icon name="key" />
         <Transition animation="scale" duration={200}>
-          <Modal open={opened} size="tiny" style={{ textAlign: 'center' }}>
-            <Modal.Header>{header}</Modal.Header>
-            <Modal.Content>
-              <Modal.Description>{helperContent}</Modal.Description>
-            </Modal.Content>
-            <Modal.Actions>
-              {action}
-              {tryAgainAction && tryAgainAction}
-            </Modal.Actions>
+          <Modal open={opened} className="public-key-modality">
+            <Modal.Content>{content}</Modal.Content>
           </Modal>
         </Transition>
       </Button>
